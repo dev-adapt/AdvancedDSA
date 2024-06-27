c59b8f1f27d0cdf5596c61803536bab032316048
dev-adapt <shrutiagnihotri1112@gmail.com>
2024-06-27 15:04:37 +0530

    27 june

--- leetcode/removeNodes.java
+++ leetcode/removeNodes.java
@@ -8,9 +8,9 @@ public class removeNodes {
               ListNode(int val) { this.val = val; }
               ListNode(int val, ListNode next) { this.val = val; this.next = next; }
           }
-          static  ListNode removeNodes(ListNode head) {
+          static  ListNode removeNode(ListNode head) {
             if (head == null) return null;
-            head.next = removeNodes(head.next);
+            head.next = removeNode(head.next);
             return head.next != null && head.val < head.next.val ? head.next : head;
           }
           public static void main(String[] args) {
