1427414f382c1647d7794ac952fb2ec95af56e35
dev-adapt <shrutiagnihotri1112@gmail.com>
2024-06-28 21:42:13 +0530

    28 june

---
+++ leetcodeJUNE/kthLargestElement.java
@@ -0,0 +1,27 @@
+package leetcodeJUNE;
+import java.util.*;
+public class kthLargestElement {
+    static int kthLargest(int nums[],int k){
+       // int n=nums.length;
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int i:nums){
+           if(pq.size()==k){
+            if(i>pq.peek()){
+            pq.remove();
+            pq.add(i);
+            
+           }
+           continue;
+        }
+        pq.add(i);
+        }
+      
+        return pq.peek();
+    }
+    public static void main(String[] args) {
+        int nums[]={3,7,2,9,5};
+        int k=3;
+        System.out.println(kthLargest(nums, k));
+    }
+
+}
