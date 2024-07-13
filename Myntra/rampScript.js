b92be3409409d5568de926bcb03830abb1f87ea2
dev-adapt <shrutiagnihotri1112@gmail.com>
2024-07-14 01:23:45 +0530

    14 july

--- Myntra/rampScript.js
+++ Myntra/rampScript.js
@@ -17,3 +17,27 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3200); // Change image every 2 seconds
 }
+function openPage() {
+    // Redirect to another page
+    window.location.href = 'theme.html';
+  }
+
+let slideIndex2 = 0;
+showSlides2();
+
+function showSlides2() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  let dots = document.getElementsByClassName("dot2");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides.length) {slideIndex2 = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";  
+  dots[slideIndex2-1].className += " active";
+  setTimeout(showSlides2, 2000); // Change image every 2 seconds
+}
