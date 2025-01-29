3212a2cdc2eeab1485ded15981496a94896723a0
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-01-29 10:26:48 +0530

    29 jan updates

---
+++ Year2025/leetcodeJan/longestSubarraySumZero.java
@@ -0,0 +1,10 @@
+public class longestSubarraySumZero {
+   public int maxLen(int arr[]) {
+        // code here
+        int ans=0;
+        int n=arr.length;
+        int target=0;
+        
+        return ans;
+    }
+}
