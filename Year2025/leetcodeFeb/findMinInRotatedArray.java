668d90d1c251f255d66637a4d6fdf3e354934deb
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-01 17:08:39 +0530

    adding codes on 1st february

---
+++ Year2025/leetcodeFeb/findMinInRotatedArray.java
@@ -0,0 +1,17 @@
+public class findMinInRotatedArray {
+    public int findMin(int[] nums) {
+        int st=0,end=nums.length-1;
+        int ans=Integer.MAX_VALUE;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]<nums[end]){ //right half is sorted
+                ans=Math.min(ans, nums[mid]);
+                end=mid-1;
+            }else{ //left half of the array is sorted
+                ans=Math.min(ans, nums[st]);
+                st=mid+1;
+            }
+        }
+        return ans;
+    }
+}
