ad8247c459bace88f8c26fe548f81c1c2fd2fbbb
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-03 14:13:16 +0530

    pushing root codes

---
+++ Year2025/leetcodeFeb/nthRoot.java
@@ -0,0 +1,27 @@
+public class nthRoot {
+    public int nthRoot(int n, int m) {
+        // code here
+         int low = 1, high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midN = func(mid, n, m);
+            if (midN == 1) {
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+        public int func(int mid, int n, int m) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
+        }
+        if (ans == m) return 1;
+        return 0;
+    }
+}
---
+++ Year2025/leetcodeFeb/sqrRoot.java
@@ -0,0 +1,13 @@
+public class sqrRoot {
+    int floorSqrt(int n) {
+        // Your code here
+        int high=n,low=1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            long val=mid*mid;
+            if(val<=(long)n)  low=mid+1;
+            else high=mid-1;
+        }
+        return high;
+    }
+}
