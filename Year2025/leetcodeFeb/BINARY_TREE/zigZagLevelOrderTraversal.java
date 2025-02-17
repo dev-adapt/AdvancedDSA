10e0e9514a795cb835dc6b12ab9c2c388519d506
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-17 12:20:35 +0530

    Auto-update: 2025-02-17 12:20:35

--- Year2025/leetcodeFeb/BINARY_TREE/zigZagLevelOrderTraversal.java
+++ Year2025/leetcodeFeb/BINARY_TREE/zigZagLevelOrderTraversal.java
@@ -17,7 +17,7 @@ public class zigZagLevelOrderTraversal {
         }
      public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         int height=height(root)+1;
-        for(int i=1:){
+        for(int i=1:height){
             if(i&1==0)
         }
     }
