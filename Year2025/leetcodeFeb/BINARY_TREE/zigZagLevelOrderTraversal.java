e6958fac431afa2fc1f0fe381a720157bd32bb42
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-17 12:17:36 +0530

    Auto-update: 2025-02-17 12:17:36

--- Year2025/leetcodeFeb/BINARY_TREE/zigZagLevelOrderTraversal.java
+++ Year2025/leetcodeFeb/BINARY_TREE/zigZagLevelOrderTraversal.java
@@ -3,6 +3,18 @@ package BINARY_TREE;
 import java.util.List;
 
 public class zigZagLevelOrderTraversal {
+    public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
      public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
     }
