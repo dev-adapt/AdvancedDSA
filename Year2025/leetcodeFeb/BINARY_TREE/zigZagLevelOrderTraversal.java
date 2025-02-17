c1b41fdc8dc23845f382a2bf5850d096cd0377f4
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-17 12:53:10 +0530

    Auto-update: 2025-02-17 12:53:10

--- Year2025/leetcodeFeb/BINARY_TREE/zigZagLevelOrderTraversal.java
+++ Year2025/leetcodeFeb/BINARY_TREE/zigZagLevelOrderTraversal.java
@@ -58,42 +58,42 @@ public class zigZagLevelOrderTraversal {
 }
 
          */
-     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>>ans=new ArrayList<List<Integer>>();
-        int len=height(root)+1;
-        if(root==null)return ans;
+    //  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    //     List<List<Integer>>ans=new ArrayList<List<Integer>>();
+    //     int len=height(root)+1;
+    //     if(root==null)return ans;
        
-        for(int i=1;i<=len;i++){
-            List<Integer>arr=new ArrayList<>();
-            if((i&1)!=0)
-            nthLevel(root, i, arr);
-            else nthLevel2(root, i, arr);
+    //     for(int i=1;i<=len;i++){
+    //         List<Integer>arr=new ArrayList<>();
+    //         if((i&1)!=0)
+    //         nthLevel(root, i, arr);
+    //         else nthLevel2(root, i, arr);
 
-            ans.add(arr);
-        }
-        return ans;
-    }
-    public int height(TreeNode root){
-        if(root == null ||(root.left==null && root.right==null)) return 0;
-        return 1 + Math.max(height(root.left), height(root.right));
-    }
-    public void nthLevel(TreeNode root,int n,List<Integer>arr){
-        if(root == null) return;
-        if(n == 1){ 
-          arr.add(root.val);
-          return;
-         }
-         nthLevel(root.left,n-1, arr);
-         nthLevel(root.right, n-1, arr);
-    }
-    public void nthLevel2(TreeNode root,int n,List<Integer>arr){
-        if(root == null) return;
-        if(n == 1){ 
-          arr.add(root.val);
-          return;
-         }
+    //         ans.add(arr);
+    //     }
+    //     return ans;
+    // }
+    // public int height(TreeNode root){
+    //     if(root == null ||(root.left==null && root.right==null)) return 0;
+    //     return 1 + Math.max(height(root.left), height(root.right));
+    // }
+    // public void nthLevel(TreeNode root,int n,List<Integer>arr){
+    //     if(root == null) return;
+    //     if(n == 1){ 
+    //       arr.add(root.val);
+    //       return;
+    //      }
+    //      nthLevel(root.left,n-1, arr);
+    //      nthLevel(root.right, n-1, arr);
+    // }
+    // public void nthLevel2(TreeNode root,int n,List<Integer>arr){
+    //     if(root == null) return;
+    //     if(n == 1){ 
+    //       arr.add(root.val);
+    //       return;
+    //      }
         
-         nthLevel(root.right, n-1, arr);
-         nthLevel(root.left,n-1, arr);
-    }
+    //      nthLevel(root.right, n-1, arr);
+    //      nthLevel(root.left,n-1, arr);
+    // }
 }
