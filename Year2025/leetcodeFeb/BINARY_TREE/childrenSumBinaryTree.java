27bd208ddabb54195cef64a17eb3643c9856c4aa
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-18 12:35:18 +0530

    Auto-update: 2025-02-18 12:35:18

--- Year2025/leetcodeFeb/BINARY_TREE/childrenSumBinaryTree.java
+++ Year2025/leetcodeFeb/BINARY_TREE/childrenSumBinaryTree.java
@@ -13,5 +13,22 @@ public class childrenSumBinaryTree {
                 this.right = right;
             }
         }
-        
+        public static int isSumProperty(Node root)
+        {
+            // add your code here
+            if(root==null)return 1;
+            if(root.left==null && root.right==null)return 1;
+            
+            int leftData = (root.left != null) ? root.left.data : 0;
+            int rightData = (root.right != null) ? root.right.data : 0;
+            
+            if(root.data == (leftData + rightData)) {
+                int left = isSumProperty(root.left);
+                int right = isSumProperty(root.right);
+                
+                if(left == 1 && right == 1) return 1;
+            }
+            return 0;
+            
+        }
 }
