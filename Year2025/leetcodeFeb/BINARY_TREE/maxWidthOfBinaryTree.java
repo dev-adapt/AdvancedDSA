5125cf74959c41a07f3c2b05831ec77758556b23
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-18 11:43:01 +0530

    Auto-update: 2025-02-18 11:43:01

--- Year2025/leetcodeFeb/BINARY_TREE/maxWidthOfBinaryTree.java
+++ Year2025/leetcodeFeb/BINARY_TREE/maxWidthOfBinaryTree.java
@@ -40,7 +40,7 @@ public class maxWidthOfBinaryTree {
                 q.poll();
 
                 if(i==0)first=curr_id;
-                if(i==size-1)
+                if(i==size-1)last=curr_id;
 
             }
         }
