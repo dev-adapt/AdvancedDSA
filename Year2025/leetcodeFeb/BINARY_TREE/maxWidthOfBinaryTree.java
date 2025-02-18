503cdcfb1b0166c3d10621bda49e95671fef3333
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-18 11:35:20 +0530

    Auto-update: 2025-02-18 11:35:19

--- Year2025/leetcodeFeb/BINARY_TREE/maxWidthOfBinaryTree.java
+++ Year2025/leetcodeFeb/BINARY_TREE/maxWidthOfBinaryTree.java
@@ -1,7 +1,18 @@
 package BINARY_TREE;
 
-public class maxWidthOfBinaryTree {
+public class maxWidthOfBinaryTree {public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     class Pair{
-        
+
     }
 }
