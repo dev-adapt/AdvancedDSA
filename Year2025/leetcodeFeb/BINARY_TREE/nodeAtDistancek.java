383cc043adf5418786a1e5a91ea8c08fba78a5c9
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-19 12:22:44 +0530

    Auto-update: 2025-02-19 12:22:44

--- Year2025/leetcodeFeb/BINARY_TREE/nodeAtDistancek.java
+++ Year2025/leetcodeFeb/BINARY_TREE/nodeAtDistancek.java
@@ -3,7 +3,18 @@ package BINARY_TREE;
 import java.util.List;
 
 public class nodeAtDistancek {
-
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
        public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         
     }
