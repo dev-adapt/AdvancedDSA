d50bf3d9d1f9cc137e0f7e1f889829671b94d85c
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-19 13:06:22 +0530

    Auto-update: 2025-02-19 13:06:22

--- Year2025/leetcodeFeb/BINARY_TREE/nodeAtDistancek.java
+++ Year2025/leetcodeFeb/BINARY_TREE/nodeAtDistancek.java
@@ -43,7 +43,9 @@ public class nodeAtDistancek {
                 }
             }
         }
-        List<Integer> ans=
+        List<Integer> ans=new List<Integer>() {
+            
+        };
     }
     private void getParent(TreeNode root, Map<TreeNode,TreeNode>parent_track){
         if(root==null) return;
