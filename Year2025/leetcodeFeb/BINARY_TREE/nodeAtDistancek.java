07a7aab44a6dff961769c1418a0ff3ece5e02b24
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-19 12:52:50 +0530

    Auto-update: 2025-02-19 12:52:50

--- Year2025/leetcodeFeb/BINARY_TREE/nodeAtDistancek.java
+++ Year2025/leetcodeFeb/BINARY_TREE/nodeAtDistancek.java
@@ -21,6 +21,7 @@ public class nodeAtDistancek {
     }
     private void getParent(TreeNode root, Map<TreeNode,TreeNode>parent_track){
         if(root==null) return;
+        
         if(root.left!=null){
             parent_track.put(root.left,root);
         }
