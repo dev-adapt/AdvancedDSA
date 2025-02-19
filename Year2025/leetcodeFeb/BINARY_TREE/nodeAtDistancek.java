1c919a48976d5cb1fa8e1a48e0fd3471ca609e56
Shruti Agnihotri <shrutiagnihotri111@gmail.com>
2025-02-19 13:04:04 +0530

    Auto-update: 2025-02-19 13:04:04

--- Year2025/leetcodeFeb/BINARY_TREE/nodeAtDistancek.java
+++ Year2025/leetcodeFeb/BINARY_TREE/nodeAtDistancek.java
@@ -37,7 +37,7 @@ public class nodeAtDistancek {
                     q.offer(curr.right);
                     visited.put(curr.right,true);
                 }
-                if()
+                if(parent_track.get(curr)!=null && visited.get(parent_track.get(curr))==null){
             }
         }
 
