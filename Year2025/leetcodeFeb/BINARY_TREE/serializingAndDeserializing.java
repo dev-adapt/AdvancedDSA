cf2421e59a51dafc8961706977864ed96c89408f
Shruti Agnihotri <shrutiagnihotri1112@gmail.com>
2025-02-20 11:01:34 +0530

    modifying serialize deserialize

--- Year2025/leetcodeFeb/BINARY_TREE/serializingAndDeserializing.java
+++ Year2025/leetcodeFeb/BINARY_TREE/serializingAndDeserializing.java
@@ -16,69 +16,57 @@ public class serializingAndDeserializing {
                 this.right = right;
             }
         }
-    public String serialize(TreeNode root) {
-       if (root == null) {
-            return "";
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-            TreeNode curNode = q.poll();
-
-            if (curNode == null) {
-                sb.append("#,");
-            } else {
-                q.offer(curNode.left);
-                q.offer(curNode.right);
+        public String serialize(TreeNode root) {
+            if (root == null) {
+                return "";
             }
+    
+            StringBuilder sb = new StringBuilder();
+            Queue<TreeNode> q = new LinkedList<>();
+            q.offer(root);
+    
+            while (!q.isEmpty()) {
+                TreeNode curNode = q.poll();
+                if (curNode == null) {
+                    sb.append("#,");
+                } else {
+                    sb.append(curNode.val).append(",");  // Append value
+                    q.offer(curNode.left);
+                    q.offer(curNode.right);
+                }
+            }
+            return sb.toString();
         }
-        return sb.toString();
-    }
+    
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+            if (data.isEmpty()) {
+                return null;
+            }
+    
+            String[] nodes = data.split(",");
+            Queue<TreeNode> q = new LinkedList<>();
+    
+            TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+            q.offer(root);
+    
+            int i = 1;
+            while (!q.isEmpty()) {
+                TreeNode node = q.poll();
+    
+                if (!nodes[i].equals("#")) {
+                    node.left = new TreeNode(Integer.parseInt(nodes[i]));
+                    q.offer(node.left);
+                }
+                i++;
+    
+                if (!nodes[i].equals("#")) {
+                    node.right = new TreeNode(Integer.parseInt(nodes[i]));
+                    q.offer(node.right);
+                }
+                i++;
+            }
+            return root;
+        }
  
-    public TreeNode deserialize(String data) {
-        if (data.isEmpty()) {
-            return null;
-        }
-
-        StringBuilder s = new StringBuilder(data);
-        //kuki values bohot badi tabhi aese comma ke basis pe pata kar rhe hai value ka
-        int commaIndex = s.indexOf(",");
-        String str = s.substring(0, commaIndex);
-        s.delete(0, commaIndex + 1);
-        TreeNode root = new TreeNode(Integer.parseInt(str));
-
-        Queue<TreeNode> q = new LinkedList<>();
-  
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-           
-            TreeNode node = q.poll();
-
-            commaIndex = s.indexOf(",");
-            str = s.substring(0, commaIndex);
-            s.delete(0, commaIndex + 1);
-       
-            if (!str.equals("#")) {
-                TreeNode leftNode = new TreeNode(Integer.parseInt(str));
-                node.left = leftNode;
-                q.offer(leftNode);
-            }
-
-            commaIndex = s.indexOf(",");
-            str = s.substring(0, commaIndex);
-            s.delete(0, commaIndex + 1);
-         
-            if (!str.equals("#")) {
-                TreeNode rightNode = new TreeNode(Integer.parseInt(str));
-                node.right = rightNode;
-                q.offer(rightNode);
-            }
-        }
-        return root;
-    }
 }
